a2ed2a7eed7ffdbfec1e3c34bfdc800666ebae98
Rasmus <rasm245r@gmail.com>
2023-03-27 23:27:03 +0200

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	import Sidebar from "../lib/sidebar.svelte";
+    import { Skeleton, Heading, Hr, Card } from 'flowbite-svelte'
+</script>
+
+
+<Sidebar />
+
+<div class=" ml-64 w-auto h-screen">
+    <Heading tag="h1" class="text-center mt-4">Alarmer</Heading>
+    <Hr class="my-4 mx-auto md:my-10" width="w-48" height="h-1"/>
+    <Card class="mx-12" horizontal>
+        <div class="w-screen">
+            <Skeleton />
+        </div>
+    </Card>
+</div>
