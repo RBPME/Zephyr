c3c5753ba21f4dbbb7aa80affe94c9a24c6cd845
RBPME <rasm245r@gmail.com>
2023-03-28 09:47:14 +0200

    made add alarm button

--- src/lib/sidebar.svelte
+++ src/lib/sidebar.svelte
@@ -1,7 +1,6 @@
 <script>
     import { page } from '$app/stores';
     import { Sidebar, SidebarGroup, SidebarItem, SidebarWrapper } from 'flowbite-svelte';
-    let spanClass = 'flex-1 ml-3 whitespace-nowrap';
     $: activeUrl = $page.url.pathname;
 </script>
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,18 @@
 <script>
-	import Sidebar from "../lib/sidebar.svelte";
-    import { Skeleton, Heading, Hr, Card } from 'flowbite-svelte'
+	import Sidebar from "$lib/sidebar.svelte";
+    import Alarm from "$lib/alarm.svelte";
+    import Add from "$lib/Add.svelte";
+    import { Heading, Hr } from 'flowbite-svelte';
 </script>
 
 
 <Sidebar />
 
-<div class=" ml-64 w-auto h-screen">
-    <Heading tag="h1" class="text-center mt-4">Alarmer</Heading>
+<div class="ml-64 w-auto h-screen pt-4">
+    <Heading tag="h1" class="text-center">Alarmer</Heading>
     <Hr class="my-4 mx-auto md:my-10" width="w-48" height="h-1"/>
-    <Card class="mx-12" horizontal>
-        <div class="w-screen">
-            <Skeleton />
-        </div>
-    </Card>
+    <div class="px-16 flex flex-col items-center">
+        <Alarm />
+        <Add />
+    </div>
 </div>
