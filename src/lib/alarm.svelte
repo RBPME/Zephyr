40f18e6251ed335a97010c122698608244594039
RBPME <rasm245r@gmail.com>
2023-03-30 12:10:53 +0200

    made bottom nav

--- src/lib/alarm.svelte
+++ src/lib/alarm.svelte
@@ -1,5 +1,5 @@
 <script>
-    import { TextPlaceholder, Heading } from "flowbite-svelte";
+    import { Skeleton, Heading } from "flowbite-svelte";
 </script>
 
 <div class="border p-6 rounded-lg bg-gray-50 dark:bg-gray-800 border-gray-200 shadow dark:border-gray-700 flex flex-row w-full">
@@ -9,7 +9,7 @@
         </Heading>
     </div>
     <div class="w-full ml-6">
-        <TextPlaceholder />
+        <Skeleton />
     </div>
     <div class="flex flex-col">
         <button><svg class="w-6 h-6 dark:fill-slate-200" xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 96 960 960" width="48"><path d="M479.858 896Q460 896 446 881.858q-14-14.141-14-34Q432 828 446.142 814q14.141-14 34-14Q500 800 514 814.142q14 14.141 14 34Q528 868 513.858 882q-14.141 14-34 14Zm0-272Q460 624 446 609.858q-14-14.141-14-34Q432 556 446.142 542q14.141-14 34-14Q500 528 514 542.142q14 14.141 14 34Q528 596 513.858 610q-14.141 14-34 14Zm0-272Q460 352 446 337.858q-14-14.141-14-34Q432 284 446.142 270q14.141-14 34-14Q500 256 514 270.142q14 14.141 14 34Q528 324 513.858 338q-14.141 14-34 14Z"/></svg></button>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,7 @@
 
 <Sidebar />
 
-<div class="ml-64 w-auto h-screen pt-4">
+<div class="md:ml-64 w-auto h-screen pt-4">
     <Heading tag="h1" class="text-center">Alarmer</Heading>
     <Hr class="my-4 mx-auto md:my-10" width="w-48" height="h-1"/>
     <div class="px-16 flex flex-col items-center">
