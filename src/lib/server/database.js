3b81652b0466d9662e50c2655adf69c4a119a4a5
RBPME <rasm245r@gmail.com>
2023-03-31 16:33:18 +0200

    began developing api

--- src/lib/server/database.js
+++ src/lib/server/database.js
@@ -1,13 +1,13 @@
 import mysql from 'mysql';
 
-export var con = mysql.createConnection({
+export var db = mysql.createConnection({
   host: "elev.vtg.dk",
   user: "ddu0823_elev_vtg",
   password: "k2jkhq31",
   database: "ddu0823_elev_vtg_dk"
 });
 
-con.connect(function(err) {
+db.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
 });
