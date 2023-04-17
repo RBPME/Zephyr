0e9c1fa52dcad7c5268769e9905eb8335993ad6d
Rasmus <rasm245r@gmail.com>
2023-04-17 19:35:12 +0200

    finished functionality of connections page

--- src/routes/Enheder/+page.server.ts
+++ src/routes/Enheder/+page.server.ts
@@ -2,41 +2,73 @@ import { fail, redirect } from "@sveltejs/kit";
 import type { PageServerLoad, Actions } from './$types';
 import { db } from '$lib/server/database';
 
-export const load: PageServerLoad = async ({ locals }) => {
+export const load: PageServerLoad = async ({ locals, cookies }) => {
     if (!locals.auth) {
         throw redirect(303, '/');
     }
 
-    let data = await db.device.findMany({
+    //get user
+    let user = await db.user.findUnique({
+        where: { userAuthToken: cookies.get('session') }
+    });
+
+    let available = await db.device.findMany({
         where: { userId: null }
     });
+
+    let conncected = await db.device.findMany({
+        where: {userId: user?.id }
+    });
+
     return {
-        row: data
+        available: available ? available : null,
+        connected: conncected ? conncected : null
     };
+
 }
 
 export const actions: Actions = {
     default: async ({ request, cookies }) => {
-        const form = await request.formData();
+        const key = await request.json();
+        console.log(request.headers.get('headerMessage'));
 
-        const key = form.get('key');
-
-        if (
-            typeof key !== 'string' ||
-            !key
-        ) {
-            return fail(400, { invalid: true });
-        }
-
-        const connectDevice = await db.user.update({
-            where: { userAuthToken: cookies.get('session') },
-            data: {
-                devices: {
-                    connect: {
-                        id: key
+        if (request.headers.get('headerMessage') == '0') {
+            if (
+                typeof key.e !== 'string' ||
+                !key.e
+            ) {
+                return fail(400, { invalid: true });
+            }
+    
+            const connectDevice = await db.user.update({
+                where: { userAuthToken: cookies.get('session') },
+                data: {
+                    devices: {
+                        connect: {
+                            id: key.e
+                        }
                     }
                 }
+            });
+        } else if (request.headers.get('headerMessage') == '1') {
+            if (
+                typeof key.e !== 'string' ||
+                !key.e
+            ) {
+                return fail(400, {invalid: true});
             }
-        });
+
+            const disconnectDevice = await db.user.update({
+                where: { userAuthToken: cookies.get('session') },
+                data: {
+                    devices: {
+                        disconnect: {
+                            id: key.e
+                        }
+                    }
+                }
+            });
+        }
+
     }
 };
--- src/routes/Enheder/+page.svelte
+++ src/routes/Enheder/+page.svelte
@@ -1,30 +1,72 @@
 <script lang="ts">
-    import type { PageData } from "./$types";
-    import Sidebar from "$lib/sidebarAccount.svelte";
-    import { Heading, Hr, Table, TableHead, TableHeadCell, TableBody, TableBodyRow, TableBodyCell } from "flowbite-svelte";
+  import type { PageData } from "./$types";
+  import Sidebar from "$lib/sidebarAccount.svelte";
+  import {
+    Heading,
+    Hr,
+    Table,
+    TableHead,
+    TableHeadCell,
+    TableBody,
+    TableBodyRow,
+    TableBodyCell,
+  } from "flowbite-svelte";
+  import { invalidateAll } from "$app/navigation";
 
-    export let data: PageData;
+  export let data: PageData;
+  console.log(data);
+
+  const call = async (e: string, del: string) => {
+    const response = await fetch("/Enheder", {
+      method: "POST",
+      body: JSON.stringify({ e }),
+      headers: { headerMessage: del },
+    });
+
+    invalidateAll();
+
+    return response;
+  };
 </script>
 
 <Sidebar />
 
 <div class="md:ml-64 w-auto h-auto pt-4">
-    <Heading tag="h1" class="text-center">Enheder</Heading>
-    <Hr class="my-4 mx-auto md:my-10" width="w-48" height="h-1" />
-    <Table class="">
-        <TableHead>
-            <TableHeadCell>Device Id</TableHeadCell>
-            <TableHeadCell>Forbind</TableHeadCell>
-        </TableHead>
-        <TableBody>
-            {#each data.row as device}
-                <TableBodyRow>
-                    <TableBodyCell>{device.id}</TableBodyCell>
-                    <TableBodyCell>
-                        
-                    </TableBodyCell>
-                </TableBodyRow>
-            {/each}
-        </TableBody>
-    </Table>
+  <Heading tag="h1" class="text-center">Enheder</Heading>
+  <Hr class="my-4 mx-auto md:my-10" width="w-48" height="h-1" />
+  <Table>
+    <TableHead>
+      <TableHeadCell>Device id</TableHeadCell>
+      <TableHeadCell>Afbryd forbindelse</TableHeadCell>
+    </TableHead>
+    <TableBody>
+      {#each data.connected as device}
+        <TableBodyRow>
+          <TableBodyCell>{device.id}</TableBodyCell>
+          <TableBodyCell>
+            <button on:click={call(device.id, "1")}>Afbryd forbindelse!</button>
+          </TableBodyCell>
+        </TableBodyRow>
+      {/each}
+    </TableBody>
+  </Table>
+  <br />
+  <br />
+  <br />
+  <Table class="">
+    <TableHead>
+      <TableHeadCell>Device id</TableHeadCell>
+      <TableHeadCell>Forbind</TableHeadCell>
+    </TableHead>
+    <TableBody>
+      {#each data.available as device}
+        <TableBodyRow>
+          <TableBodyCell>{device.id}</TableBodyCell>
+          <TableBodyCell>
+            <button on:click={call(device.id, "0")}>Forbind!</button>
+          </TableBodyCell>
+        </TableBodyRow>
+      {/each}
+    </TableBody>
+  </Table>
 </div>
